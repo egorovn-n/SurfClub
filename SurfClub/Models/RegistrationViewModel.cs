58638c1d222398476baebf4add46a56efe03c33a
Nikolay <egorov_nikolay_n@mail.ru>
2022-07-11 14:00:52 +0400

    fourth day last commit

--- SurfClub/Models/RegistrationViewModel.cs
+++ SurfClub/Models/RegistrationViewModel.cs
@@ -4,16 +4,20 @@ namespace SurfClub.Models
 {
     public class RegistrationViewModel
     {
+        [Required, MinLength(3, ErrorMessage = "Псевдоним должен содержать более 3 символов"), MaxLength(20)]
         [Display(Name = "Псевдоним*")]
         public string UserName { get; set; }
 
+        [Required, MaxLength(31)]
         [Display(Name = "Почта*")]
         public string Email { get; set; }
 
+        [Required, MinLength(6, ErrorMessage = "Пароль должен содержать более 3 символов"), MaxLength(20)]
         [Display(Name = "Пароль*")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Required, MinLength(6, ErrorMessage = "Пароль должен содержать более 3 символов"), MaxLength(20)]
         [Display(Name = "Подтвердите пароль*")]
         [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
@@ -27,12 +31,15 @@ namespace SurfClub.Models
         [Display(Name = "Выберите фото")]
         public Guid? SelectPhoto { get; set; }
 
+        [MaxLength(255)]
         [Display(Name = "Контактная информация")]
         public string? ContactInfo { get; set; }
 
+        [MaxLength(255)]
         [Display(Name = "О себе")]
         public string? About { get; set; }
 
+        [MaxLength(255)]
         [Display(Name = "Достижения")]
         public string? Achievements { get; set; }
     }
